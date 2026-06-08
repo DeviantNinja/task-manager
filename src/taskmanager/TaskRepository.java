ee60a723a834127d0c14fa107bfc5eca50c4dd2d
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2026-06-08 21:20:54 +0100

    added javadoc comments

--- src/taskmanager/TaskRepository.java
+++ src/taskmanager/TaskRepository.java
@@ -1,4 +1,22 @@
 package taskmanager;
 
+import java.util.List;
+
+/**
+ * Defines the operations required to store and retrieve tasks.
+ *
+ * Implementations may use different storage mechanisms,
+ * such as files, database or in-memory collections.
+ */
 public interface TaskRepository {
+    /**
+     * Returns all tasks stored in the repository.
+     * @return a list containing all stored tasks
+     */
+    List<Task> getTasks();
+
+    /**
+     * Saves all tasks to underlying storage mechanism
+     */
+    void saveTasks();
 }
