2d1e0ae5b5eb7777c3358ee4da0ac9cc3e66e8da
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2026-06-08 19:14:20 +0100

    Created initial project structure

---
+++ src/java/taskmanager/Main.java
@@ -0,0 +1,10 @@
+package taskmanager;
+
+/**
+ * Entry point for Task Manager Application
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task Manager Started!");
+    }
+}
