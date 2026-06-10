0600e38d370dd7a06264ae328555c001ba511795
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2026-06-10 21:25:56 +0100

    updated Main to start the task manager, and start the UI on launch

--- src/taskmanager/Main.java
+++ src/taskmanager/Main.java
@@ -1,10 +1,17 @@
 package taskmanager;
 
+import taskmanager.UI.CLI.ConsoleUI;
+import taskmanager.service.TaskManager;
+
 /**
  * Entry point for Task Manager Application
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task Manager Started!");
+        TaskManager taskManager = new TaskManager();
+        ConsoleUI ui = new ConsoleUI(taskManager);
+        ui.start();
+
     }
 }
