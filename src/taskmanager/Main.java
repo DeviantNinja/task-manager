c75e5c23b9be2c754052c833676517982c309395
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2026-06-11 17:12:52 +0100

    updated main to use fileTaskRepository instead of memoryTaskRepository

--- src/taskmanager/Main.java
+++ src/taskmanager/Main.java
@@ -1,16 +1,19 @@
 package taskmanager;
 
 import taskmanager.UI.CLI.ConsoleUI;
+import taskmanager.repository.FileTaskRepository;
 import taskmanager.repository.MemoryTaskRepository;
 import taskmanager.repository.TaskRepository;
 import taskmanager.service.TaskManager;
+
+import java.nio.file.Paths;
 
 /**
  * Entry point for Task Manager Application
  */
 public class Main {
     public static void main(String[] args) {
-        TaskRepository repository = new MemoryTaskRepository();
+        TaskRepository repository = new FileTaskRepository(Paths.get("c:\\Users\\d_joh\\Desktop\\tasks.txt"));
         TaskManager taskManager = new TaskManager(repository);
         ConsoleUI ui = new ConsoleUI(taskManager);
         ui.start();
