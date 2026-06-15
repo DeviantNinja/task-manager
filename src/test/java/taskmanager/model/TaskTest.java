070d5333516e9cdb94b244cf5b04d4399bc0f01c
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2026-06-15 19:25:56 +0100

    added test classes to ensure JUNIT functionality

---
+++ src/test/java/taskmanager/model/TaskTest.java
@@ -0,0 +1,11 @@
+package taskmanager.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TaskTest {
+    @Test
+    void taskManagerTest() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/taskmanager/service/TaskManagerTest.java
@@ -0,0 +1,10 @@
+package taskmanager.service;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TaskManagerTest {
+    @Test
+    void taskManagerTest() {
+        assertTrue(true);
+    }
+}
